d613fcfcd59be917b1f8ed3f038140331bd9cba0
Moshe Malka <moshemal@gmail.com>
2016-08-01 15:26:46 +0300

    adding clone-repos and setup-kernel

---
+++ scripts/clone-repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+
+
+mkdir -p testim
+cd testim
+git clone https://github.com/testimio/clickim.git
+git clone https://github.com/testimio/runner.git
+git clone https://github.com/testimio/services.git
+
+
