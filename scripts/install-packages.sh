a24f6ee67aa9d9bf94c5fcbd8ecc170be7efa8ff
Moshe Malka <moshemal@gmail.com>
2016-08-02 19:02:53 +0300

    adding curl to package list

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -2,7 +2,7 @@
 #gir1.2-gtop-2.0 for Multi-Core System Monitor 1.5
 #dconf-cli for keyboard shortcuts
 packages=(libcurl3 build-essential htop mc git gitk git-gui virtualbox vim gedit dropbox \
-	nemo-dropbox gir1.2-gtop-2.0 dconf-cli)
+	nemo-dropbox gir1.2-gtop-2.0 dconf-cli fonts-liberation)
 
 echo installing packages ${packages[*]}
 sudo apt-get install -y ${packages[*]}
