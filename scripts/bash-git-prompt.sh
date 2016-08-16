89ceb1268cd2c68b485f81967c21a7d87603d153
Moshe Malka <moshemal@gmail.com>
2016-08-16 20:01:22 +0300

    adding bash-git-prompt

---
+++ scripts/bash-git-prompt.sh
@@ -0,0 +1,5 @@
+#############################################################################
+# custom git prompt cloned from https://github.com/magicmonty/bash-git-prompt
+#############################################################################
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+cat bash-git-prompt.bashrc >> ~/.bashrc
